64e070990857e730a5cb10133f059758277d078f
Kumaravel <masterdslakauxid@gmail.com>
2023-10-15 23:27:41 +0530

    Adding first set of files

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import time
+import random
+
+st.header("Group1 - Capstone project")
+title = st.text_input('Activity', 'Archery')
+st.write('The current movie title is', title.upper())
