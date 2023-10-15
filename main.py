e0939e5a59d45662f8c1e8466e5ca14ed826efdb
Kumaravel <masterdslakauxid@gmail.com>
2023-10-16 00:16:06 +0530

    added validation for the text

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import random
 
 st.header("Group1 - Capstone project")
 title = st.text_input('Activity', 'Archery')
+
 if st.button('Submit'):
-    st.write('The  keyword to search is .', title.upper())
+    if len(title) > 0:
+        if len(title) < 6:
+            st.error("Activity name should be at least 5 charaters")
+        else:
+            st.write('The  keyword to search is .', title.upper())
+    else: 
+        st.error("Activity to search is missing")
+
+    
 
